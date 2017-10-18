26277f84561276e88de4f395a3dedb06a13517f7
Alexandru Bala <alexandrubala94@gmail.com>
2017-10-18 19:18:10 +0300

    error free

--- sensors.js
+++ sensors.js
@@ -4,5 +4,5 @@ function ondeviceorientation(event)
 { 
     document.getElementById("id_alpha").innerHTML = event.alpha;
     document.getElementById("id_beta").innerHTML = event.beta;
-    document.getElementById("id_gama").innerHTML = event.gamma;
+    document.getElementById("id_gamma").innerHTML = event.gamma;
 }
