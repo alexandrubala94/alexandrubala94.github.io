5011227bdd3c8788528711f0bb35e373ac0926d2
Alexandru Bala <alexandrubala94@gmail.com>
2017-10-18 19:15:00 +0300

    citeste senzori telefon

---
+++ sensors.js
@@ -0,0 +1,8 @@
+window.addEventListener('deviceorientation', ondeviceorientation);
+
+function ondeviceorientation(event) 
+{ 
+    document.getElementById("id_alpha").innerHTML = event.alpha;
+    document.getElementById("id_beta").innerHTML = event.beta;
+    document.getElementById("id_gama").innerHTML = event.gamma;
+}
